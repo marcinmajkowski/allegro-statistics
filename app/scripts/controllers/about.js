eac942818981c47c91a8884b38cb17d55224cec9
Marcin Majkowski <pl.marcin.m@gmail.com>
2016-03-18 23:28:40 +0100

    Fix broken grunt build by adding missing modules

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name januszeMotoryzacjiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the januszeMotoryzacjiApp
+ */
+angular.module('januszeMotoryzacjiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
