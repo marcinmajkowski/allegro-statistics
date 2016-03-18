fdd3a461b98d392e81baa9fda795c5a4a1725072
Marcin Majkowski <pl.marcin.m@gmail.com>
2016-03-18 21:42:40 +0100

    Init using generator-angular

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name januszeMotoryzacjiApp
+ * @description
+ * # januszeMotoryzacjiApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('januszeMotoryzacjiApp', [
+    'ngAnimate'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name januszeMotoryzacjiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the januszeMotoryzacjiApp
+ */
+angular.module('januszeMotoryzacjiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
