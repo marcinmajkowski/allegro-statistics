66997d9a57ba78d40f5f2a878436e486fcbd9076
Marcin Majkowski <pl.marcin.m@gmail.com>
2016-03-19 08:36:40 +0100

    Implement back button in history tree

--- app/scripts/controllers/category.js
+++ app/scripts/controllers/category.js
@@ -10,8 +10,22 @@
 angular.module('januszeMotoryzacjiApp')
   .controller('CategoryCtrl', function ($scope, category) {
     $scope.categories = category.query();
+    $scope.topLevel = true;
+
+    var history = {};
 
     $scope.enter = function(index) {
+      history = {
+        history: history,
+        categories: $scope.categories
+      };
       $scope.categories = $scope.categories[index].subcategories;
-    }
+      $scope.topLevel = history.history === undefined;
+    };
+
+    $scope.back = function() {
+      $scope.categories = history.categories;
+      history = history.history;
+      $scope.topLevel = history.history === undefined;
+    };
   });
